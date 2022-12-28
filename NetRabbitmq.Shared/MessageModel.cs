d96c0b750a855affbdfc1278c411b8985c7a38d4
Marcos Bastos <marcos.abf@gmail.com>
2022-12-28 12:03:57 -0300

    set to init

--- NetRabbitmq.Shared/MessageModel.cs
+++ NetRabbitmq.Shared/MessageModel.cs
@@ -2,10 +2,10 @@
 {
     public class MessageModel
     {
-        public Guid Id { get; private set; } = Guid.NewGuid();
-        public DateTime SendTime { get; private set; } = DateTime.Now;
-        public string From { get; private set; } = Environment.MachineName;
-        public string? Body { get; set; }
+        public Guid Id { get; init; } = Guid.NewGuid();
+        public DateTime SendTime { get; init; } = DateTime.Now;
+        public string From { get; init; } = Environment.MachineName;
+        public string? Body { get; init; }
         public MessageModel(string body)
         {
             Body = body;
